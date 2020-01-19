cc4ad931022e66b0a6b865775f63f20e94d78edb
vibin230 <vibin2303@gmail.com>
2020-01-19 15:17:10 +0530

    home page name

--- portfolio.js
+++ portfolio.js
@@ -4,6 +4,7 @@ const leftTri = document.querySelector(".leftTri");
 const rightTri = document.querySelector(".rightTri");
 const midTri = document.querySelector(".midTri");
 const navbar = document.querySelector(".navbar");
+const headerTxt = document.querySelector(".headerTxt");
 
 const onloadAnimation = () => {
     return (
@@ -15,6 +16,7 @@ const onloadAnimation = () => {
                 midTri.classList.add("mTriAnimation");
                 setTimeout(() => {
                     loadingScreen.classList.add("d-none");
+                    headerTxt.classList.remove("d-none");
                 }, 500)
             }, 1000)
         }, 1000)
