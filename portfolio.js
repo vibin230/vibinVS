74ff85f071459821e26342f7d8725ca7cab20a9a
vibin230 <vibin2303@gmail.com>
2020-02-13 05:34:16 +0530

    working on home buttons

--- portfolio.js
+++ portfolio.js
@@ -5,6 +5,9 @@ const rightTri = document.querySelector(".rightTri");
 const midTri = document.querySelector(".midTri");
 const navbar = document.querySelector(".navbar");
 const headerTxt = document.querySelector(".headerTxt");
+const homeButtons = document.querySelector(".btnGroup");
+var aboutMe = true;
+const listAcitve = document.querySelector(".abtMe");
 
 const onloadAnimation = () => {
     return (
@@ -17,11 +20,18 @@ const onloadAnimation = () => {
                 setTimeout(() => {
                     loadingScreen.classList.add("d-none");
                     headerTxt.classList.remove("d-none");
+                    homeButtons.classList.remove("d-none");
                 }, 500)
             }, 1000)
         }, 1000)
 
     );
+}
+
+const aboutMeactive = () => {
+    if(aboutMe==true) aboutMe=false;
+    else if (aboutMe == false) aboutMe = true;
+    aboutMe ? (listAcitve.classList.add(".listItemActive"), listAcitve.classList.remove(".listItemDefault")): (listAcitve.classList.remove(".listItemActive"), listAcitve.classList.add("listItemDefault"));
 }
 
 window.onload = onloadAnimation;
